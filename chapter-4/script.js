a73794d6e4321d50f9433121f2f81d34c49ea4df
Michael <michael@dropsolid.com>
2015-07-13 18:04:19 +0200

    first exercise

--- chapter-4/script.js
+++ chapter-4/script.js
@@ -1,12 +1,35 @@
-console.log('minimum');
+console.log('The sum of a range');
+console.log('--------');
+
+var arr = [];
+
+var range = function range(start, end) {
+  for (var a = start; a <= end ; a++ ) {
+    arr.push(a);
+  }
+  return arr;
+}
+
+var sum = function sum(arr) {
+  var x = 0;
+  for (var a = 0; a <= arr.length -1; a++) {
+    x = x + arr[a];
+  }
+  return x;
+}
+
+console.log(range(1,10));
+console.log(sum(arr));
+
+console.log('');
+console.log('Reversing an array');
 console.log('--------');
 
 
 console.log('');
-console.log('recursion');
+console.log('A list');
 console.log('--------');
 
-
 console.log('');
-console.log('Bean counting');
+console.log('Deep Comparison');
 console.log('--------');
